3e10b7000536b9f25b7213871db513808fbb5959
baixiaocun <809097453@qq.com>
2021-08-30 14:53:56 +0800

    项目初始化

---
+++ src/db/EasyDbException.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @name:easyDb异常
+ *
+ * 功能介绍
+ * @author   hexu
+ * @date 2021/8/29 2:47 下午
+ */
+
+namespace Guest\EasyCom\db;
+
+
+class EasyDbException extends \RuntimeException
+{
+
+}
